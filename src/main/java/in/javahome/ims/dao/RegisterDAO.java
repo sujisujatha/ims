75ea44fee3d278c15af95f7ce3bbf90c57246578
javahome <hari.kammana@gmail.com>
2016-01-21 15:36:00 +0530

    FindByObject Bug Fix

--- src/main/java/in/javahome/ims/dao/RegisterDAO.java
+++ src/main/java/in/javahome/ims/dao/RegisterDAO.java
@@ -3,6 +3,7 @@ package in.javahome.ims.dao;
 import in.javahome.ims.pojo.RegisterModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -10,12 +11,22 @@ import org.springframework.stereotype.Repository;
 public class RegisterDAO implements IRegisterDAO {
 	@Autowired
 	private JdbcTemplate template;
+
 	public void register(RegisterModel model) {
 		String sql = "INSERT INTO STUDENTS_INFO(FIRST_NAME,LAST_NAME,MOBILE,MAIL,REFFERED_BY,YEAR_OF_PASS_OUT) VALUES(?,?,?,?,?,?)";
-		template.update(sql, model.getFirstName(),model.getLastName(),model.getMobile(),model.getEmail(),model.getRefferedBy(),model.getYearOfPassOut());
+		template.update(sql, model.getFirstName(), model.getLastName(),
+				model.getMobile(), model.getEmail(), model.getRefferedBy(),
+				model.getYearOfPassOut());
 	}
+
 	public String findRegisteredStudent(String email) {
 		String sql = "SELECT MAIL FROM STUDENTS_INFO WHERE MAIL=?";
-		return template.queryForObject(sql,new Object[]{email}, String.class);
+		try {
+			return template.queryForObject(sql, new Object[] { email },
+					String.class);
+		} catch (DataAccessException e) {
+			return null;
+		}
+
 	}
 }
