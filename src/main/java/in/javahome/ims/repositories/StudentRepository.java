1de24bed7cb9f0889471eb7e4b150c06928de245
Kammana <hari@javahome.in>
2016-10-28 10:29:33 +0530

    learning git fro ecllipse

--- src/main/java/in/javahome/ims/repositories/StudentRepository.java
+++ src/main/java/in/javahome/ims/repositories/StudentRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer>{
-	public Student findByEmail(String email);
+//	public Student findByEmail(String email);
 }
